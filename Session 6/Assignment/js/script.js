f6c13dfc3cf249b085afcf2e474c60a242aa81c6
Mustafa Shaaban <mustafashaaban22@gmail.com>
2020-07-15 23:36:48 +0200

    building task

---
+++ Session 6/Assignment/js/script.js
@@ -0,0 +1,23 @@
+/** -----------------------------------------------------------------------------------
+ *
+ *   Package Name: Session 6 Assignment 6 - Main Script File
+ *   Version: 1.0
+ *   Author: Mustafa Shaaban
+ *
+ *   OBJECTS INDEX
+ *   ===================
+ *   01. UICtr
+ *      The object which contain the most important elements to be loaded once
+ *
+ *   02. main
+ *      The object which contain the front page functions
+ *
+ *
+ ----------------------------------------------------------------------------------*/
+
+function isEven(value) {
+    if (value % 2 == 0)
+        return true;
+    else
+        return false;
+}
