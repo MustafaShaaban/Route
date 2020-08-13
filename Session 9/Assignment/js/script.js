e9ad2bd61d9deebf9923ea9cc0b86ce9b4179e2a
Mustafa Shaaban <mustafashaaban22@gmail.com>
2020-08-13 23:09:46 +0200

    Assignment 9/Bookmark version 1.0

--- Session 9/Assignment/js/script.js
+++ Session 9/Assignment/js/script.js
@@ -1,108 +1,187 @@
 (function () {
 
-    var siteName    = document.querySelector('#site_name'),
-        siteUrl     = document.querySelector('#site_url'),
-        submitBtn   = document.querySelector('#addBookmark'),
-        siteNameErr = document.querySelector('#site_name_err'),
-        siteUrlErr  = document.querySelector('#site_url_err');
+    window.onload = function () {
+        DB.initDB();
+        view.updateView();
+    };
 
-    var tblBody   = document.querySelector('#tableBody'),
-        visitBtn  = _('.visitBtn'),
-        editBtn   = _('.editBtn'),
-        deleteBtn = _('.removeBtn');
+    let siteName    = _('#site_name'),
+        siteUrl     = _('#site_url'),
+        submitBtn   = _('#addBookmark'),
+        siteNameErr = _('#site_name_err'),
+        siteUrlErr  = _('#site_url_err');
 
-
-    (function createDB() {
-        if (localStorage.getItem('bookmarks') === null) {
-            localStorage.setItem('bookmarks', JSON.stringify([]));
-        }
-    })();
-
-    function _(el) {
-        return document.querySelectorAll(el).length > 0 ? Array.from(document.querySelectorAll(el)) : [];
-    }
-
+    let tblBody   = _('#tableBody'),
+        visitBtn  = _all('.visitBtn'),
+        editBtn   = _all('.editBtn'),
+        deleteBtn = _all('.removeBtn');
 
     const DB = {
+        initDB: function () {
+            if (localStorage.getItem('bookmarks') === null) {
+                localStorage.setItem('bookmarks', JSON.stringify([]));
+            }
+        },
+
         get: function (id) {
-            var data = this.getAll();
-            console.log(data[id]);
+            let data = this.getAll();
+            return data[id];
         },
 
         getAll: function () {
             return JSON.parse(localStorage.getItem('bookmarks'));
         },
 
+        isExist: function () {
+
+        },
+
         insert: function (bookmarkObj) {
-            var data = this.getAll();
+            let data = this.getAll();
             data.push(bookmarkObj);
             localStorage.setItem('bookmarks', JSON.stringify(data));
         },
 
-        update: function () {
+        update: function (bookmarkObj) {
+            let data = this.getAll();
+            data[bookmarkObj.id] = bookmarkObj;
+            localStorage.setItem('bookmarks', JSON.stringify(data));
+        },
 
+        delete: function (id) {
+            let data = this.getAll();
+            data.splice(id, 1);
+            localStorage.setItem('bookmarks', JSON.stringify(data));
+        }
+    };
+
+    const view = {
+        init: function () {
+            this.visitBtn();
+            this.editBtn();
+        },
+
+        createRow: function (data) {
+            let template = `
+                <tr>
+                    <td>${data.name}</td>
+                    <td>
+                        <div class="controllers">
+                            <a href="javascript:" class="btn btn-info visitBtn" data-id="${data.id}">Visit</a>
+                            <a href="javascript:" class="btn btn-warning editBtn" data-id="${data.id}">Edit</a>
+                            <a href="javascript:" class="btn btn-danger removeBtn" data-id="${data.id}">Remove</a>
+                        </div>
+                    </td>
+                </tr>
+            `;
+            return template;
+        },
+
+        updateView: function () {
+            let template = '',
+                all      = DB.getAll();
+            for (let i = 0; i < all.length; i++) {
+                let bookmarkObj = {
+                    id: i,
+                    status: all[i].status,
+                    name: all[i].name,
+                    url: all[i].url
+                };
+                template += this.createRow(bookmarkObj);
+            }
+            tblBody.innerHTML = template;
+            CRUD.init();
+            this.init();
+        },
+
+        visitBtn: function () {
+            for (let i = 0; i < _all('.visitBtn').length; i++) {
+                _all('.visitBtn')[i].addEventListener('click', function (e) {
+                    e.preventDefault();
+                    let $this    = e.currentTarget,
+                        bookmark = DB.get($this.getAttribute('data-id'));
+                    window.open(bookmark.url, '_blank');
+                });
+            }
+        },
+
+        editBtn: function () {
+            for (let i = 0; i < _all('.editBtn').length; i++) {
+                _all('.editBtn')[i].addEventListener('click', function (e) {
+                    e.preventDefault();
+                    let $this    = e.currentTarget,
+                        bookmark = DB.get($this.getAttribute('data-id'));
+                    siteName.focus();
+                    siteName.value = bookmark.name;
+                    siteUrl.value = bookmark.url;
+                    _('#addBookmark').style.display = "none";
+                    _('#editBookmark').style.display = "block";
+                    _('#editBookmark').setAttribute('data-id', $this.getAttribute('data-id'));
+                });
+            }
+        }
+    };
+
+    const CRUD = {
+        init: function () {
+            this.add();
+            this.edit();
+            this.delete();
+        },
+
+        add: function () {
+            _('#addBookmark').addEventListener('click', function (e) {
+                e.preventDefault();
+                if (siteName.value !== '' && siteUrl.value !== '') {
+                    let bookmarkObj = {
+                        status: 'publish',
+                        name: siteName.value,
+                        url: siteUrl.value
+                    };
+                    DB.insert(bookmarkObj);
+                    view.updateView();
+                    restForm();
+
+                }
+            });
+        },
+
+        edit: function () {
+            _('#editBookmark').addEventListener('click', function (e) {
+                e.preventDefault();
+                if (siteName.value !== '' && siteUrl.value !== '') {
+                    let $this = e.currentTarget,
+                        bookmarkID = $this.getAttribute('data-id');
+
+                    let bookmarkObj = {
+                        id: bookmarkID,
+                        status: 'modified',
+                        name: siteName.value,
+                        url: siteUrl.value
+                    };
+
+                    DB.update(bookmarkObj);
+                    view.updateView();
+                    restForm();
+                }
+            });
         },
 
         delete: function () {
-
+            for (let i = 0; i < _all('.removeBtn').length; i++) {
+                _all('.removeBtn')[i].addEventListener('click', function (e) {
+                    e.preventDefault();
+                    let $this      = e.currentTarget,
+                        bookmarkID = $this.getAttribute('data-id');
+                    DB.delete(bookmarkID);
+                    view.updateView();
+                });
+            }
         }
+
+
     };
 
-    function displayBookmarks() {
-        var template = '',
-            all      = DB.getAll();
-        for (var i = 0; i < all.length; i++) {
-            template += `
-            <tr>
-                <td>${all[i].name}</td>
-                <td>
-                    <div class="controllers">
-                        <a href="javascript:" class="btn btn-info visitBtn" data-id="${i}">Visit</a>
-                        <a href="javascript:" class="btn btn-warning editBtn" data-id="${i}">Edit</a>
-                        <a href="javascript:" class="btn btn-danger removeBtn" data-id="${i}">Remove</a>
-                    </div>
-                </td>
-            </tr>
-        `;
-        }
-        tblBody.innerHTML = template;
-    };
-
-    displayBookmarks();
-
-    submitBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-        if (siteName.value !== '' && siteUrl.value !== '') {
-            var bookmarkObj = {
-                name: siteName.value,
-                url: siteUrl.value
-            };
-            DB.insert(bookmarkObj);
-            displayBookmarks();
-            restForm();
-
-        }
-
-
-    });
-
-    // visitBtn.addEventListener('click', function (e) {
-    //     e.preventDefault();
-    // });
-    //
-    // editBtn.addEventListener('click', function (e) {
-    //     e.preventDefault();
-    // });
-
-    for (var i = 0; i < deleteBtn.length; i++) {
-        deleteBtn[i].addEventListener('click', function (e) {
-            e.preventDefault();
-            var $this      = e.currentTarget,
-                bookmarkID = $this.getAttribute('data-id');
-
-            DB.get(bookmarkID);
-        });
-    }
 
     siteName.addEventListener('keyup', validateEmptyInp);
 
@@ -111,21 +190,17 @@
 
     function validateEmptyInp(e) {
         e.preventDefault();
-        var $this     = e.currentTarget,
+        let $this     = e.currentTarget,
             errAreaID = $this.getAttribute("data-err");
 
         if ($this.value === '') {
-            document.querySelector(errAreaID).style.cssText = 'display: block !important';
+            _(errAreaID).style.cssText = 'display: block !important';
         } else {
-            document.querySelector(errAreaID).style.cssText = 'display: none !important';
+            _(errAreaID).style.cssText = 'display: none !important';
         }
-        submitBtn.disabled = siteName.value === '' || siteUrl.value === '';
-    }
 
-    function restForm() {
-        siteName.value = '';
-        siteUrl.value = '';
-        submitBtn.disabled = true;
+        _('#addBookmark').disabled = siteName.value === '' || siteUrl.value === '';
+        _('#editBookmark').disabled = siteName.value === '' || siteUrl.value === '';
     }
 
 
